f020a49bab76d1c7db73dd82e860457bdf9e2a6e
Tarun Mandalapu <t.mandalapu@gmail.com>
2018-02-18 21:58:31 -0600

    Added jdoc comments

--- src/BalancedSearchTree.java
+++ src/BalancedSearchTree.java
@@ -10,11 +10,22 @@
 // Known Bugs:       TODO
 ///////////////////////////////////////////////////////////////////////////////
 
+/**
+ * 
+ * @author tmand
+ * Binary search tree implementation using AVL tree algorithm. 
+ * Implements methods from SearchTreeADT given for assignment.
+ * @param <T> generic comparable object for use as keys in search tree.
+ */
 public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeADT<T> {
-    protected int numNodes;
-    protected Treenode<T> root;
+    protected int numNodes; //Keeps track of the number of nodes in the tree
+    protected Treenode<T> root; //Keeps track of the tree's root node
     
-	// inner node class used to store key items and links to other nodes
+    /**
+     * @author tmand
+     * Class for nodes in the binary search tree
+     * @param <K> generic comparable object for key
+     */
 	protected class Treenode<K extends Comparable<K>> {
 	    K key;
         Treenode<K> left;
@@ -29,7 +40,12 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 			this.right = right;
 		}
 	}
-
+	
+	/**
+	 * Method used to rebalance tree
+	 * Rotates nodes one to the right with "b" as a focal point
+	 * @param b, the node we are rotating from
+	 */
 	@SuppressWarnings("unused")
     private void rotateRight(Treenode<T> b)
 	{
@@ -42,6 +58,11 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 	    }
 	}
 	
+	/**
+     * Method used to rebalance tree
+     * Rotates nodes one to the left with "a" as a focal point
+     * @param a, the node we are rotating from
+     */
 	@SuppressWarnings("unused")
 	private void rotateLeft(Treenode<T> a)
 	{
@@ -54,11 +75,17 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 	    }
 	}
 	
+	/**
+	 * Returns a string containing the keys of each node in the tree in ascending order seperated by commas
+	 */
 	public String inAscendingOrder() {
 		//TODO : must return comma separated list of keys in ascending order
 		return "" ;
 	}
 
+	/**
+	 * Returns true if the tree is empty and false otherwise
+	 */
 	public boolean isEmpty() {
 		if(numNodes == 0)
 		{
@@ -67,21 +94,33 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 		return false;
 	}
 
+	/**
+	 * Returns the height of the tree
+	 */
 	public int height() {
 		//TODO return the height of this tree
 		return 0; 
 	}
 
+	/**
+	 * Returns true if the tree contains a node with item as its key
+	 */
 	public boolean lookup(T item) {
 		//TODO must return true if item is in tree, otherwise false
 		return false;
 	}
 
+	/**
+	 * Inserts a new node with item as its key in the correct position
+	 */
 	public void insert(T item) {
 		//TODO if item is null throw IllegalArgumentException, 
 		// otherwise insert into balanced search tree
 	}
 
+	/**
+	 * If the tree contains a node with item as its key, remove it from the tree and rebalance
+	 */
 	public void delete(T item) {
 		//TODO if item is null or not found in tree, return without error
 		// else remove this item key from the tree and rebalance
