f0890fd61428dff4f50e0e449b58adc65d5f1035
Matt Zimmers <matthewzimmers@gmail.com>
2018-02-18 22:41:26 -0600

    DuplicateKeyException class
    Created it from extending RuntimeException

---
+++ src/DuplicateKeyException.java
@@ -0,0 +1,11 @@
+
+public class DuplicateKeyException extends RuntimeException {
+
+    public DuplicateKeyException() {
+        super();
+    }
+    
+    public DuplicateKeyException(String e) {
+        super(e);
+    }
+}
